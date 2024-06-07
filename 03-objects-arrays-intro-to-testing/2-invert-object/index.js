a5522dfbca60ece7a94e7e66520b3a8fc93cfdb8
Andrey Miskov <amiskov@ya.ru>
2024-06-07 08:22:06 +0300

    add 3.2 invert-object solution

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (!obj) {
+        return undefined;
+    }
+    const res = {};
+    for (const [key, value] of Object.entries(obj)) {
+        res[value] = key
+    }
+    return res
 }
