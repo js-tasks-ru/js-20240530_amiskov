818a3c1687587ae74143235123e09333e30281c8
Andrey Miskov <amiskov@ya.ru>
2024-06-12 14:46:51 +0300

    add 4.1 column-chart solution

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,69 @@
 export default class ColumnChart {
+    element;
+    chartHeight = 50;
+    #chartElement;
+    #loadingClass = "column-chart_loading";
 
+    constructor(options = {}) {
+        const { data = [] } = options;
+
+        const elementHtml = this.renderElement(options);
+        this.element = this.createElement(elementHtml);
+        this.#chartElement = this.element.querySelector(".column-chart__chart");
+
+        this.update(data);
+    }
+
+    renderElement({ label, link, value, formatHeading = h => h }) {
+        const totalValue = formatHeading(value);
+        const linkHTML = link ? `<a class="column-chart__link" href="${link}">View all</a>` : "";
+        return `
+            <div class="column-chart__title">Total ${label}${linkHTML}</div>
+            <div class="column-chart__container">
+                <div data-element="header" class="column-chart__header">${totalValue}</div>
+                <div data-element="body" class="column-chart__chart"></div>
+            </div>
+        `;
+    }
+
+    createElement(innerHtml) {
+        const el = document.createElement("div");
+        el.classList.add("column-chart");
+        el.innerHTML = innerHtml;
+        return el;
+    }
+
+    update(data) {
+        if (data.length === 0) {
+            this.element.classList.add(this.#loadingClass);
+        } else {
+            this.element.classList.remove(this.#loadingClass);
+        }
+        this.#chartElement.innerHTML = this.renderChart(data, this.chartHeight);
+    }
+
+    renderChartBar(value, maxValue, scale) {
+        const percent = (value / maxValue * 100).toFixed(0);
+        const barHeight = Math.floor(value * scale);
+        return `<div style="--value: ${barHeight}" data-tooltip="${percent}%"></div>`
+    }
+
+    renderChart(data) {
+        if (data.length === 0) {
+            return `<img src="./charts-skeleton.svg">`;
+        }
+        const maxValue = Math.max(...data);
+        const scale = this.chartHeight / maxValue;
+        return data
+            .map(value => this.renderChartBar(value, maxValue, scale))
+            .join("");
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 }
