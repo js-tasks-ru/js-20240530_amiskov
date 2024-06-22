e904943f28a96b59174e8620b5dd278dc86c5ed6
Andrey Miskov <amiskov@ya.ru>
2024-06-22 10:06:34 +0300

    add 5.1 solution

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  static instance = null;
 
+  constructor(message = "", {type = "success", duration = 2000} = {}) {
+    this.type = type;
+    this.duration = duration;
+    this.message = message;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  getRootElement() {
+    if (!NotificationMessage.instance) {
+      NotificationMessage.instance = document.createElement('div');
+    }
+    return NotificationMessage.instance;
+  }
+
+  createElement(html) {
+    const el = this.getRootElement();
+    el.style.cssText = `--value: ${this.duration / 1000}s`;
+    el.innerHTML = html;
+    el.classList.value = "notification"; // keep only `notification`
+    el.classList.add(this.type);
+    return el;
+  }
+
+  show(target) {
+    if (target) {
+      target.appendChild(this.element);
+    } else {
+      document.body.appendChild(this.element);
+    }
+
+    clearTimeout(this.timeoutId);
+    this.timeoutId = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+
+  }
+
+  createTemplate() {
+    return `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+    `;
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timeoutId);
+    NotificationMessage.instance = null;
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
